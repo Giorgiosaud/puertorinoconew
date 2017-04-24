eb61c8fc6cc3b424369ad423717c7533d0bbe4f9
Jorge Saud <jorgelsaud@gmail.com>
2017-04-24 14:01:22 -0400

    origin page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="hero is-success is-large">
+<section class="hero is-success is-medium">
   <div class="hero-body">
     <div class="container">
       <h1 class="title">
