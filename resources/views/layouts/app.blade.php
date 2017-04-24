4db31cb70223b6845b0e9c1260a445d09945d245
Jorge Saud <jorgelsaud@gmail.com>
2017-04-24 13:55:47 -0400

    origin page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,74 +22,23 @@
 	</head>
 	<body>
 		<div id="app">
-			<section class="hero is-primary is-medium">
-				<!-- Hero header: will stick at the top -->
-				<div class="hero-head">
-					<header class="nav">
-						<div class="container">
-							<div class="nav-left">
-								<a class="nav-item">
-									<img src="images/bulma-type-white.png" alt="Logo">
-								</a>
-							</div>
-							<span class="nav-toggle">
-								<span></span>
-								<span></span>
-								<span></span>
-							</span>
-							<div class="nav-right nav-menu">
-								<a class="nav-item is-active">
-									Home
-								</a>
-								<a class="nav-item">
-									Examples
-								</a>
-								<a class="nav-item">
-									Documentation
-								</a>
-								<span class="nav-item">
-									<a class="button is-primary is-inverted">
-										<span class="icon">
-											<i class="fa fa-github"></i>
-										</span>
-										<span>Download</span>
-									</a>
-								</span>
-							</div>
-						</div>
-					</header>
-				</div>
-
-				<!-- Hero content: will be in the middle -->
-				<div class="hero-body">
-					<div class="container has-text-centered">
-						<h1 class="title">
-							Title
-						</h1>
-						<h2 class="subtitle">
-							Subtitle
-						</h2>
+			@yield('content')
+			<footer class="footer">
+				<div class="container">
+					<div class="content has-text-centered">
+						<p>
+							<strong>Bulma</strong> by <a href="http://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+							<a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+							is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+						</p>
+						<p>
+							<a class="icon" href="https://github.com/jgthms/bulma">
+								<i class="fa fa-github"></i>
+							</a>
+						</p>
 					</div>
 				</div>
-
-				<!-- Hero footer: will stick at the bottom -->
-				<div class="hero-foot">
-					<nav class="tabs">
-						<div class="container">
-							<ul>
-								<li class="is-active"><a>Overview</a></li>
-								<li><a>Modifiers</a></li>
-								<li><a>Grid</a></li>
-								<li><a>Elements</a></li>
-								<li><a>Components</a></li>
-								<li><a>Layout</a></li>
-							</ul>
-						</div>
-					</nav>
-				</div>
-			</section>
-
-			@yield('content')
+			</footer>
 		</div>
 
 		<!-- Scripts -->
