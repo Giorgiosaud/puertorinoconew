6a95b3be7c036f30b03b51820298316d98bfb1b8
Jorge Saud <jorgelsaud@gmail.com>
2017-04-24 14:37:51 -0400

    origin page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,56 +29,50 @@
 				<div class="field">
 					<label class="label">E-Mail Address</label>
 					<p class="control has-icons-left has-icons-right">
-					<input id="email" class="input {{ $errors->has('email') ? 'is-danger' : '' }} " type="email" placeholder="Email input" value="{{ old('email') }}" required autofocus>
+						<input id="email" class="input {{ $errors->has('email') ? 'is-danger' : '' }} " type="email" placeholder="Email input" value="{{ old('email') }}" required autofocus>
 						<span class="icon is-small is-left">
 							<i class="fa fa-envelope"></i>
 						</span>
+						@if($errors->has('email'))
 						<span class="icon is-small is-right">
 							<i class="fa fa-warning"></i>
 						</span>
-					</p>
-					<p class="help is-danger">{{ $errors->first('email') }}/p>
-				</div>
-
-				<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-					<label for="password" class="col-md-4 control-label">Password</label>
-
-					<div class="col-md-6">
-						<input id="password" type="password" class="form-control" name="password" required>
-
-						@if ($errors->has('password'))
-						<span class="help-block">
-							<strong>{{ $errors->first('password') }}</strong>
-						</span>
 						@endif
-					</div>
+					</p>
+					@if($errors->has('email'))
+					<p class="help is-danger">{{ $errors->first('email') }}</p>
+					@endif
+				</div>
+				<div class="field">
+					<label class="label">Password</label>
+					<p class="control">
+						<input class="input {{ $errors->has('password') ? 'is-danger' : '' }}" type="password" placeholder="Password" name="password">
+					</p>
+					@if($errors->has('password'))
+					<p class="help is-danger">{{ $errors->first('password') }}</p>
+					@endif
+				</div>
+				<div class="field">
+					<p class="control">
+						<label class="checkbox">
+							<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+							Remember Me
+						</label>
+					</p>
 				</div>
 
-				<div class="form-group">
-					<div class="col-md-6 col-md-offset-4">
-						<div class="checkbox">
-							<label>
-								<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-							</label>
-						</div>
-					</div>
+				<div class="field is-grouped">
+					<p class="control">
+						<button class="button is-primary">Login</button>
+					</p>
+					<p class="control">
+						<a class="button is-link" href="{{ route('password.request') }}">Forgot Your Password?</a>
+					</p>
 				</div>
+		</form>
 
-				<div class="form-group">
-					<div class="col-md-8 col-md-offset-4">
-						<button type="submit" class="btn btn-primary">
-							Login
-						</button>
-
-						<a class="btn btn-link" href="{{ route('password.request') }}">
-							Forgot Your Password?
-						</a>
-					</div>
-				</div>
-			</form>
-
-		</div>
 	</div>
+</div>
 </section>
 
 @endsection
