425fc2358362380c7a6b52890b160739af48d64b
Jorge Saud <jorgelsaud@gmail.com>
2017-04-24 21:49:51 -0400

    test login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,10 +40,10 @@ class LoginController extends Controller
 	public function validateLogin(Request $request){
 		if(filter_var($request->input('login'), FILTER_VALIDATE_EMAIL)) {
 			$request->request->add(['email' => $request->input('login')]);
-			$request->request->remove(['login']);
+			// $request->request->remove(['login']);
 		}
 		$request->request->add(['username' => $request->input('login')]);   
-		$request->request->remove(['login']);
+		// $request->request->remove(['login']);
 		$this->validate($request, [
             $this->username() => 'required', 'password' => 'required',
         ]);
