67d760870b0621153e53d627cbfb816900e84eb2
Jorge Saud <jorgelsaud@gmail.com>
2017-04-24 21:46:17 -0400

    test login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,39 +9,50 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+	/*
+	|--------------------------------------------------------------------------
+	| Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles authenticating users for the application and
+	| redirecting them to your home screen. The controller uses a trait
+	| to conveniently provide its functionality to your applications.
+	|
+	*/
 
-    use AuthenticatesUsers;
-    // public function login(Request $request){
-        // dd($request);
-    // }
+	use AuthenticatesUsers;
+	// public function login(Request $request){
+		// dd($request);
+	// }
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+	/**
+	 * Where to redirect users after login.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function login(Request $request){
-        dd($request->all());
-    }
-    public function __construct()
-    {
-        $this->middleware('guest', ['except' => 'logout']);
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function username(Request $request){
+		if($request->isset('email')){
+			return 'email';
+		}
+		if($request->isset('username')){
+			return 'username';
+		}
+		if(filter_var($request->input('login'), FILTER_VALIDATE_EMAIL)) {
+			$request->request->add(['email' => $request->input('login')]);
+			return 'email';
+		}
+		$request->request->add(['username' => $request->input('login')]);   
+		return 'username';
+	}
+	public function __construct()
+	{
+		$this->middleware('guest', ['except' => 'logout']);
+	}
 }
